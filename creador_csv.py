ebd66b5895d08450aff9be9f4368e9cbffe46b54
ilitteri <ilitteri@fi.uba.ar>
2020-06-28 22:49:24 +0200

    Comente dentro de cada funcion una ayuda para entender el funcionamiento de dicha funcion

--- creador_csv.py
+++ creador_csv.py
@@ -1,9 +1,21 @@
 def leer_archivo_principal(archivo_principal):
+    '''
+    Lee el archivo principal que le llega por parametro (en nuestro caso el .txt), y retorna una lista con las lineas 
+    de ese archivo (en este caso cada linea corresponde a las ubicaciones de los archivos de la aplicacion a a anlizar).
+    '''
     with open(archivo_principal, "r") as archivo:
         lineas = archivo.read().splitlines()
     return lineas
 
 def leer_programas(archivo_principal):
+    '''
+    Analiza cada uno de los archivos que se encuentran en el archivo principal (que se pasa por parametro) y devuelve
+    un diccionario ordenado con todos los datos de esos archivos con la forma:
+    datos_programas ---->funcion_1 ---->{"modulo": modulo_func_1, "parametros": param_func_1, "lineas":lineas_cod_func_1, "comentarios": coment_func_1}
+                    ---->funcion_2 ---->{"modulo": modulo_func_2, "parametros": param_func_2, "lineas":lineas_cod_func_2, "comentarios": coment_func_2}
+                            ...
+                    ---->funcion_n ---->{"modulo": modulo_func_n, "parametros": param_func_n, "lineas":lineas_cod_func_n, "comentarios": coment_func_n}
+    '''
     ubicaciones = leer_archivo_principal(archivo_principal)
     u_programa_principal = ubicaciones[0]
     datos_programas = {}
@@ -27,6 +39,11 @@ def leer_programas(archivo_principal):
     return datos_programas
 
 def guardar_datos():
+    '''
+    Imprime los datos en un archivo .csv que creamon en la misma. Los datos se imprimen en la forma que se pide en
+    la consigna.
+    Se crea un archivo de fuente y un archivo de comentarios, para cada archivo analizado en la funcion anterior
+    '''
     datos = leer_programas("programas.txt")
     nombres_funciones = list(datos.keys())
     modulos = list(set([datos[nombre_funcion]["modulo"] for nombre_funcion in nombres_funciones]))
