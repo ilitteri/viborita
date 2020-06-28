7c738b6f7ac92d23b8bd1e0a0cf8829ce063ad52
ilitteri <ilitteri@fi.uba.ar>
2020-06-28 23:16:08 +0200

    Se agregan comentarios a todo el codigo actual

--- creador_csv.py
+++ creador_csv.py
@@ -16,26 +16,41 @@ def leer_programas(archivo_principal):
                             ...
                     ---->funcion_n ---->{"modulo": modulo_func_n, "parametros": param_func_n, "lineas":lineas_cod_func_n, "comentarios": coment_func_n}
     '''
+    #Lista de ubicaciones de modulos de la aplicacion
     ubicaciones = leer_archivo_principal(archivo_principal)
+    #Ubicacion del modulo principal
     u_programa_principal = ubicaciones[0]
     datos_programas = {}
 
+    #Recorre la lista de ubicaciones de cada archivo de la aplicacion
     for ubicacion in ubicaciones:
+        #Nombre del modulo
         nombre_modulo = ubicacion.split("\\")[-1]
+        #Abro el archivo con la ubicacion en la que se encuentra en la iteracion
         with open(ubicacion, "r") as codigo:
+            #Lee la primer linea del archivo que abri
             linea = codigo.readline()
+            #Entra al while siempre y cuando no llegue a la ultima linea del codigo del archivo
             while linea:
+                #Si la linea empieza con def, entonces se trata de una funcion, por lo tanto:
                 if linea.startswith("def"):
+                    #Deja solo el nombre de la funcion con su/sus parametro/s
                     recorte = linea.strip()[4:-1]
+                    #Obtiene el nombre de la funcion del anterior recorte
                     nombre_funcion = recorte.split("(")[0]
+                    #Obtiene los parametros de la funcion del anterior recorte
                     parametros = recorte.split("(")[1].split(")")[0]
+                    #Guarda los datos en un diccionario general, cada funcion es una key y su value son "sus caracteristicas"
                     datos_programas[nombre_funcion] = {"modulo": nombre_modulo, "parametros": f'({parametros})', "lineas": [], "comentarios": []}
+                #Filtra comentarios
                 if (len(datos_programas) > 0) and linea.startswith("    ") and ("#" not in linea or "'''" not in linea):
                     datos_programas[nombre_funcion]["lineas"].append(linea.strip())
+                #Filtra las lineas de codigo
                 elif linea.strip().startswith("#") or linea.strip().startswith("'''"):
                     datos_programas[nombre_funcion]["comentarios"].append(linea.strip())
+                #Lee la siguiente linea del codigo
                 linea = codigo.readline()
-
+    #Devuelve el diccionario, con la forma que se explica al principio de la funcion
     return datos_programas
 
 def guardar_datos():
@@ -44,15 +59,25 @@ def guardar_datos():
     la consigna.
     Se crea un archivo de fuente y un archivo de comentarios, para cada archivo analizado en la funcion anterior
     '''
+    #Diccionario con los datos de todos los programas
     datos = leer_programas("programas.txt")
+    #Lista de nombres de funciones
     nombres_funciones = list(datos.keys())
+    #Lista de todos los modulos
     modulos = list(set([datos[nombre_funcion]["modulo"] for nombre_funcion in nombres_funciones]))
+    #Recorre cada modulo
     for modulo in modulos:
+        #Crea 2 archivos .csv con el nombre del modulo
         with open(f'fuente_{modulo}.csv', "w") as archivo_fuente, open(f'comentarios_{modulo}.csv', "w") as archivo_comentarios:
+            #Recorre funcion por funcion
             for nombre_funcion in nombres_funciones:
+                #Si el modulo de la iteracion actual corresponde al modulo de la funcion de la iteracion actual:
                 if modulo == datos[nombre_funcion]["modulo"]:
+                    #Imprime una linea del .csv fuente correspondiente al modulo
                     archivo_fuente.write(f'{nombre_funcion},{datos[nombre_funcion]["parametros"]},{datos[nombre_funcion]["modulo"]},{datos[nombre_funcion]["lineas"]}\n')
+                    #Si la funcion tiene comentarios:
                     if len(datos[nombre_funcion]["comentarios"]) > 0:
+                        #Escribe una linea en el .csv de comentarios correspondiente al modulo
                         archivo_comentarios.write(f'{nombre_funcion},nombre de autor,ayuda,{datos[nombre_funcion]["comentarios"]}\n')
 
 guardar_datos()
