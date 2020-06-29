d4f0de45e8ad01679031212635beca974275276b
Andreskub <akubler@fi.uba.ar>
2020-06-28 22:34:34 -0300

    Agregue un menu de interaccion al archivo para poder dividir los puntos del TP

--- creador_csv.py
+++ creador_csv.py
@@ -84,4 +84,30 @@ def guardar_datos():
                         #Escribe una linea en el .csv de comentarios correspondiente al modulo
                         archivo_comentarios.write(f'{nombre_funcion},nombre de autor,ayuda,{datos[nombre_funcion]["comentarios"]}\n')
 
-guardar_datos()
+
+def main():
+    print("¡Binevenido!\nEste programa consta en analizar y evaluar programas escritos en Python\n")
+    guardar_datos()
+
+    bandera = True
+    while bandera:
+        print("Menú de interacción\n1. Panel general de funciones\n2. Consulta de funciones\n3. Analizador de reutilización de codigo\n4. Arbol de invocación\n5. Información por desarrollador\n6. Salir")
+
+        opcion = input("Ingrese una opcion:")
+        if opcion == "1":
+            print("Aca va el punto 1\n")
+        elif opcion == "2":
+            print("Aca va el punto 2\n")
+        elif opcion == "3":
+            print("Aca va el punto 3\n")
+        elif opcion == "4":
+            print("Aca va el punto 4\n")
+        elif opcion == "5":
+            print("Aca va el punto 5\n")
+        elif opcion == "6":
+            print("\nGracias, vuelva pronto!")
+            bandera = False
+        else:
+            print("\n¡LA OPCION INGRESADA ES INCORRECTA!\n")
+
+main()
