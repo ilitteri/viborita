f9b33b8aec16c8bd316a3a0c081f4705348fca79
Ivan Litteri (FIUBA) <ilitteri@fi.uba.ar>
2020-07-02 03:03:13 +0200

    Comento el codigo

--- creador_csv.py
+++ creador_csv.py
@@ -5,10 +5,11 @@ def obtener_ubicaciones_modulos(archivo_principal):
     Lee el archivo principal que le llega por parametro (en nuestro caso el .txt), y retorna una lista con las lineas 
     de ese archivo (en este caso cada linea corresponde a las ubicaciones de los archivos de la aplicacion a a anlizar).
     '''
-
+    #Abro el archivo y obtengo una lista de todas sus lineas
     with open(archivo_principal, "r") as archivo:
         ubicaciones = archivo.read().splitlines()
 
+    #Devuelvo la lista de lineas 
     return ubicaciones
 
 def separar_linea_funcion(linea_codigo):
@@ -19,17 +20,25 @@ def separar_linea_funcion(linea_codigo):
     bandera = False
     nombre_funcion = ""
     parametros = ""
+
+    #Recorro caracter por caracter la linea que me llega por parametro
     for caracter in linea_codigo[3:-2]:
+        #Cuando el caracter es "(" se habilita la bandera para guardar los caracteres en el string parametros 
         if caracter == "(":
             bandera = True
+        #Filtra caracteres que corresponden a espacios
         if not caracter.isspace():
+            #Si la bandera esta habilitada, se guradan los caracteres en parametros
             if bandera:
                 parametros += caracter
+            #Si la bandera no esta habilitada, se guardan los caracteres en nombre_funcion
             else:
                 nombre_funcion += caracter
+        #Cuando el caracter es ")" se deshabilita la bandera
         if caracter == ")":
             bandera = False
 
+    #Devuelvo el nombre de la funcion y sus parametros
     return nombre_funcion, parametros
 
 def leer_codigo(codigo, datos_actuales, nombre_modulo, imports):
@@ -46,10 +55,11 @@ def leer_codigo(codigo, datos_actuales, nombre_modulo, imports):
     linea_codigo = codigo.readline()
     #Entra al while siempre y cuando no llegue a la ultima linea del codigo del archivo
     while linea_codigo:
+        #SI la linea empieza con un return o si encuentro otro def, significa que termino la funcion anterior, entonces resetea la bandera y el contador
         if linea_codigo.strip().startswith("return") or contador_def == 2:
             contador_def = 0
             bandera_funcion = False
-        #Si la linea empieza con def, entonces se trata de una funcion, por lo tanto:
+        #Si la linea empieza con def, entonces se trata de una funcion, entonces habilita la bandera y obtiene el nombre de la funcion y sus parametros
         if linea_codigo.startswith("def"):
             bandera_funcion = True
             nombre_funcion, parametros = separar_linea_funcion(linea_codigo)
@@ -67,25 +77,35 @@ def leer_codigo(codigo, datos_actuales, nombre_modulo, imports):
             #Si el nombre del modulo no esta como key, entonces lo agrega (Esto ocurre una vez sola)
             if nombre_modulo not in imports:
                 imports[nombre_modulo] = []
-        if bandera_funcion:
             imports[nombre_modulo].append(linea_codigo)
+        #Si la bandera esta habilitada (estoy dentro de una funcion)
+        if bandera_funcion:
+            #Filtro las lineas que puedan llegar a tratarse de diccionarios
             if "#" not in linea_codigo and "'''" not in linea_codigo and not bandera_comentario:
                 datos_actuales[nombre_funcion]["lineas"].append(f'"{linea_codigo.strip()}"')
+            #Filtro los comentarios del tipo "#" y las lineas que no sean comentarios para analizar los comentarios de comillas triples
+            #Entran las lineas si se comienza un comentario de comillas triples y no se cierra en la misma lina
             elif linea_codigo.startswith(" ") and linea_codigo.strip().startswith("'''") and linea_codigo.count("'''") == 1:
                 contador_comillas_triples += 1
+                #Habilita la bandera que hara que se guarden las lineas siguientes hasta que se deshabilite la bandera (cuando se encuentran las comillas que cierran el comentario)
                 bandera_comentario = True
+                #Cuando se cierra el comentario de comillas triples se deshabilita la bandera y se vuelve el contador a 0 en caso de que haya otro comentario de este estilo
                 if contador_comillas_triples == 2:
                     contador_comillas_triples = 0
                     bandera_comentario = False
-            elif linea_codigo.startswith(" ") and linea_codigo.count("'''") == 2:
+            #Se guarda la linea si se empieza y termina un comentario de comillas triples en la misma linea y ademas se trate de la linea con la informacion del autor de la funcion
+            elif linea_codigo.startswith(" ") and linea_codigo.count("'''") == 2 and "Autor" in linea_codigo:
                 datos_actuales[nombre_funcion]["comentarios"]["autor"] = f'"{linea_codigo.strip()[3:-3]}"'
+            #Se guardan las lineas que corresponden a comentarios que comienzan con "#"
             elif linea_codigo.strip().startswith("#"):
                 datos_actuales[nombre_funcion]["comentarios"]["otros comentarios"].append(f'"{linea_codigo.strip()}"')
+            #Si la bandera esta habilitada y aun no se encontraron las comillas que cierran el comentario, sumo las lineas al string de ayuda de la funcion
             if bandera_comentario and contador_comillas_triples < 2:
                 datos_actuales[nombre_funcion]["comentarios"]["ayuda"] += f'{linea_codigo.strip()}'
         #Lee la siguiente linea del codigo
         linea_codigo = codigo.readline()
 
+    #Devuelvo un diccionario de datos de todos los modulos, y uno de imports
     return datos_actuales, imports
 
 def grabar_fuente_individual(archivo_fuente, nombre_funcion, parametros_funcion, nombre_modulo, lineas_codigo):
@@ -171,9 +191,8 @@ def borrar_archivos_csv_individuales(nombres_archivos_csv_individuales):
     de una funcion a la que le llega por parametro los nombres de los archivos .csv individuales
     '''
 
-    #Obtengo las ubicaciones
-    ubicaciones_archivos_csv_individuales = obtener_ubicaciones_archivos_csv_individuales(nombres_archivos_csv_individuales)
-    for ubicacion_archivo_csv_individual in ubicaciones_archivos_csv_individuales:
+    #Obtengo las ubicaciones y las recorro para borrar el archivo que se encuentra en ella
+    for ubicacion_archivo_csv_individual in obtener_ubicaciones_archivos_csv_individuales(nombres_archivos_csv_individuales):
         #Borro el archivo que se encuentra en esa ubicacion
         os.remove(ubicacion_archivo_csv_individual)
 
