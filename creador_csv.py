6283f530e830d59dde448f43785d27be0dd94909
Ivan Litteri (FIUBA) <ilitteri@fi.uba.ar>
2020-07-05 22:17:17 +0200

    Comento las nuevas funciones

--- creador_csv.py
+++ creador_csv.py
@@ -11,49 +11,92 @@ def obtener_ubicaciones_modulos(archivo_principal):
     return ubicaciones
 
 def analizar_linea_funcion(line, bandera_nombre = True, bandera_parametro = False):
+    '''[Autor: Ivan Litteri]
+    [Ayuda: esta funcion analiza la linea que le entra por parametro, previamente filtrada como linea de declaracion de funcion, de esta forma
+    se recorre caracter a caracter la linea para ver cuando guardar los caracteres en la cadena de nombre, y cuando guardar los caracteres en 
+    la cadena de parametros]'''
+
+    #Declara inicialmente cadenas vacias para luego llenarla desde 0
     nombre_funcion = ""
     parametros_funcion = ""
+
+    #Recorre caracter a caracter desde "def" en adelante la linea que le llega por parametro
     for caracter in line[3:]:
+        #Se guardan los caracteres en la cadena de parametros cuando esta habilitada la bandera
         if bandera_parametro:
             parametros_funcion += caracter
+        #Se guardan los caracteres en la cadena de nombre cuando esta habilitada la bandera y siempre y cuando no se trate de espacios
         if not caracter.isspace() and caracter != "(" and bandera_nombre:
             nombre_funcion += caracter
+        #Cuando aparece el caracter evaluado, se habilita la bandera para empezar a guardar los caracteres en la cadena de parametros y se deshabilita la bandera que permitia el almacenamiento de caracteres en la cadena de nombre
         elif caracter == "(":
             parametros_funcion += caracter
             bandera_nombre = False
             bandera_parametro = True
+        #Se deshabilita la bandera que permitia el almacenamiento de caracteres en la cadena de parametros cuando se detecta que ya no se trata mas de uno
         elif caracter == ")":
             bandera_parametro = False
 
     return nombre_funcion, parametros_funcion
 
 def analizar_linea_autor(linea_codigo, bandera_autor = False):
+    '''[Autor: Ivan Litteri]
+    [Ayuda: Esta funcion analiza la linea de codigo que anteriormente fue filtrada como posible contenedora de datos del autor de la funcion.
+    Se analiza caracter a caracter la linea, y se habilita la bandera que permite que se empiecen a almacenar los caracteres que son parte de
+    la informacion requerida.]'''
+
+    #Declara inicialmente como una cadena vacia para luego llenarla desde 0
     autor_funcion = ""
+
+    #Recorre caracter a caracter la linea que le entra por parametro
     for caracter in linea_codigo.strip():
+        #Cuando se termine el comentario de autor, se deshabilita la bandera para que no se continuen guardando caracteres en la cadena.
         if caracter == "]" and bandera_autor:
             bandera_autor = False
         if bandera_autor:
             autor_funcion += caracter
+        #Cuando el caracter se trate de una apertura de corchete y al mismo tiempo esa linea tenga la palabra autor, se habilita la bandera para que se almacenen caracteres en la cadena de autor.
         if caracter == "[" and "Autor" in linea_codigo:
             bandera_autor = True
 
     return autor_funcion
 
 def analizar_linea_ayuda(linea_codigo, bandera_ayuda):
+    '''[Autor: Ivan Litteri]
+    [Ayuda: Esta funcion analiza la linea de codigo que le llega por parametro (sabiendo que se trata de una linea de ayuda de funcion;
+    se recorre la linea caracter por caracter hasta hayar la apertura de un corchete que al mismo tiempo en esa linea este la palabra ayuda,
+    si ese fuera el caso, se habilita la bandera para que cada caracter se sume a la cadena inicializada al principio como vacia. Esta funcion
+    en particular, tambien devuelve el ultimo estado de la bandera de ayuda, porque al tratarse de comentarios multilinea, cuando se vuelva
+    a llamar a esta funcion, puede ser que este comentario multilinea no haya sido cerrado, en esta caso, deben seguir siendo almacenadas
+    las proximas lineas que vengan a ella por parametro hasta que se cierre el comentario.]'''
+
+    #Declara inicialmente como una cadena vacia para luego llenarla desde 0
     ayuda_funcion = ""
+
+    #Recorre caracter a caracter la linea que entra por parametro
     for caracter in linea_codigo.strip():
+        #Cuando se termine el comentario de ayuda, se deshabilita la bandera para que no se continuen guardando caracteres en la cadena.
         if caracter == "]" and bandera_ayuda:
             bandera_ayuda = False
         if bandera_ayuda:
             ayuda_funcion += caracter
+        #Cuando el caracter se trate de una apertura de corchete y al mismo tiempo esa linea tenga la palabra ayuda, se habilita la bandera para que se almacenen caracteres en la cadena de ayuda.
         if caracter == "[" and "Ayuda" in linea_codigo:
             bandera_ayuda = True
 
     return ayuda_funcion, bandera_ayuda
 
 def analizar_comentario_numeral(linea_codigo, bandera_otro_comentario = False):
+    '''[Autor: Ivan Litteri]
+    [Ayuda: Esta funcion analiza la linea que le llega por parametro (sabiendo que se trata de una linea que posiblemente tenga un comentario
+    de linea simple, y devuelve solo desde el "#" en adelante)]'''
+
+    #Declara inicialmente como una cadena vacia para luego llenarla desde 0
     otro_comentario = ""
+
+    #Recorre caracter a caracter la linea que entra por parametro
     for caracter in linea_codigo.strip():
+        #Cuando el caracter se trate del numeral, se habilita la bandera para que se empiece a guardar caracteres en la cadena inicializada anteriormente
         if caracter == "#":
             bandera_otro_comentario = True
         if bandera_otro_comentario:
@@ -61,27 +104,41 @@ def analizar_comentario_numeral(linea_codigo, bandera_otro_comentario = False):
 
     return otro_comentario
 
-def leer_codigo(codigo, datos_ordenados, nombre_modulo, imports, bandera_funcion = False, bandera_comentario = False, bandera_ayuda = False):
-    nombre_funcion = None
+def leer_codigo(codigo, datos_ordenados, nombre_modulo, imports, bandera_funcion = False, bandera_comentario = False, bandera_ayuda = False, nombre_funcion = None):
+    '''[Autor: Ivan Litteri]
+    [Ayuda: Lee el codigo que le llega por parametro, lo analiza con distintas funciones y actualiza el 
+    diccionario donde se guardan los datos analizados cada vez que se llama.]'''
+
     linea_codigo = codigo.readline()
     while linea_codigo:
+        #Se habilita esta bandera cuando en el codigo a leer empieza una funcion, se deshabilita cuando termina o empieza otra
         if bandera_funcion:
+            #Se habilita esta bandera cuando se detecta un comentario multilinea que no se cierra en la misma linea
             if bandera_comentario:
                 ayuda_funcion, bandera_ayuda = analizar_linea_ayuda(linea_codigo, bandera_ayuda)
                 datos_ordenados[nombre_funcion]["comentarios"]["ayuda"] += ayuda_funcion
+                #Se deshabilita la bandera cuando se detecta que se cierra el comentario multilinea.
                 if "'''" in linea_codigo:
                     bandera_comentario = False
+            #Guarda otro tipo de comentarios
             elif "#" in linea_codigo:
+                #Si es None, cambia su valor a una lista vacia para que se pueda hacer append.
                 if datos_ordenados[nombre_funcion]["comentarios"]["otros"] == None:
                     datos_ordenados[nombre_funcion]["comentarios"]["otros"] = []
                 otro_comentario = analizar_comentario_numeral(linea_codigo)
                 datos_ordenados[nombre_funcion]["comentarios"]["otros"].append(f'"{otro_comentario}"')
+            #Si un comentario multilinea se abre y cierra en la misma linea, analiza la linea y guarda los datos del autor.
             elif linea_codigo.count("'''") == 2:
-                datos_ordenados[nombre_funcion]["comentarios"]["autor"] = linea_codigo.strip()[4:-4]
+                autor_funcion = analizar_linea_autor(linea_codigo)
+                datos_ordenados[nombre_funcion]["comentarios"]["autor"] = autor_funcion
+            #Si la linea empieza con un comentario multilinea, y no se cierra en la misma linea, se analiza esta primera linea que corresponde 
+            #al autor, y luego habilita la bandera de comentario multilinea para que se analicen las lineas siguientes hasta que se cierre 
+            #el comentario multilinea.
             elif linea_codigo.strip().startswith("'''"):
                 bandera_comentario = True
                 autor_funcion = analizar_linea_autor(linea_codigo)
                 datos_ordenados[nombre_funcion]["comentarios"]["autor"] = autor_funcion
+            #Si ninguna linea es un comentario guarda la linea en lineas de codigo
             else:    
                 datos_ordenados[nombre_funcion]["lineas"].append(f'"{linea_codigo.strip()}"')
 
@@ -169,7 +226,6 @@ def crear_archivos_csv_individuales(ubicaciones_modulos):
                 if nombre_modulo == datos_modulos[nombre_funcion]["modulo"]:
                     grabar_fuente_individual(archivo_fuente, nombre_funcion, datos_modulos[nombre_funcion]["parametros"], nombre_modulo, datos_modulos[nombre_funcion]["lineas"])
                     grabar_comentarios_individual(archivo_comentarios, nombre_funcion, datos_modulos[nombre_funcion]["comentarios"])
-                print(datos_modulos[nombre_funcion]["lineas"])
 
 #EN CONSTRUCCION
 def aparear_archivos(nombres_archivos_csv_individuales):
