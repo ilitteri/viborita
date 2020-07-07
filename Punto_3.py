cb5635516094b5d1070ab763aad639bc0e0a167a
lfaguilera <67346702+lfaguilera@users.noreply.github.com>
2020-07-07 01:36:41 -0300

    Comento lo que tengo de codigo asi aclaro que hace cada cosa

--- Punto_3.py
+++ Punto_3.py
@@ -1,35 +1,38 @@
 
 def obtencion_datos(archivo_fuente):
-    """
-    Abro el .csv generado en creador_csv.py y creo un diccionario 
-    que contiene una lista de las invocaciones de cada fincion
-    """
+    
+    #Abro el .csv generado en creador_csv.py 
     with open ("archivo_fuente", "r") as informacion:
         funciones = informacion.readline()
         diccionario_invocaciones = {}
+        #Creo un diccionario que contiene una lista de las invocaciones de cada funcion
         while funciones :
-            nombre = funciones.splitline(",")[0]
+            nombre = funciones.splitline("','")[0]
             diccionario_invocaciones[nombre] = []
-            lineas = funciones.splitline(",")[3:]
+            #Filtro las lineas que no son de codigo
+            lineas = funciones.splitline("','")[3:]
             for linea in lineas :
                 for funcion in linea :
-                    if "import" in funcion :   
+                    if "import" in funcion :
+                        #Busco las invocaciones dentro de las lineas de codigo   
                         invocado = linea.splitline("import")[1]
+                        #Agrego un if para agregar invocaciones de varias funciones en un mismo import
                         if "," in invocado :
                             invocaciones = invocado.split(",")
+                            for funcion_invocada in invocaciones :
+                                diccionario_invocaciones[nombre].append(funcion_invocada) 
                         else :
-                            invocaciones = invocado
-                        diccionario_invocaciones[nombre].append(invocaciones)                  
+                                diccionario_invocaciones[nombre].append(invocaciones)                  
             funciones = informacion.readline()
+            #Devuelvo el diccionario 
     return diccionario_invocaciones
 
 def contar_interacciones(diccionario_invocaciones):
-    """
-    Creo un nuevo diccionario que tiene las funciones sin repetir 
-    y su numero de llamadas
-    """
+    
+    #Creo un nuevo diccionario que tiene las funciones sin repetir 
     diccionario_funciones = {}
     for funcion in diccionario_invocaciones :
+        #Cuento su numero de llamadas
         for invocacion in diccionario_invocaciones[funcion]:
             numero_llamadas = diccionario_invocaciones[funcion].count(invocacion)
             diccionario_funciones[funcion][invocacion] =  numero_llamadas
