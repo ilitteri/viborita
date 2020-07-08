dcb619e8351b85617d85d14beda74e2dd1d362f7
Ivan Litteri (FIUBA) <ilitteri@fi.uba.ar>
2020-07-09 00:07:37 +0200

    Termino el punto arreglando la funcion que obtiene el porcentaje de lineas totales de codigo por autor

--- informacion_desarrollador.py
+++ informacion_desarrollador.py
@@ -12,9 +12,10 @@ def leer_archivos_csv(nombre_archivo_fuente, nombre_archivo_comentarios):
     
     return datos_ordenados
 
-def obtener_porcentaje_lineas_codigo(total_lineas, lineas):
+def obtener_porcentaje_lineas_codigo(datos, lineas_por_autor):
     '''[Autor: Ivan Litteri]'''
-    return (lineas / total_lineas) * 100
+    return (lineas_por_autor / sum(datos[autor][nombre_funcion] for autor in datos for nombre_funcion in datos[autor])) * 100
+
 
 def grabar_linea(archivo_datos, linea):
     '''[Autor: Ivan Litteri]'''
@@ -23,27 +24,29 @@ def grabar_linea(archivo_datos, linea):
 def imprimir_datos(datos_por_autor, nombre_archivo_participacion):
     '''[Autor: Ivan Litteri]
     [Ayuda: imprime una tabla con la informacion de desarrollo por cada autor en la consola y en un archivo de texto]'''
+    
     archivo_datos = open(nombre_archivo_participacion, "w")
-    lineas_totales = 0
+    lineas_totales_codigos = 0
 
     print("\t\t\tInformacion de Desarrollo Por Autor\n")
     grabar_linea(archivo_datos, "\t\t\tInformacion de Desarrollo Por Autor\n\n")
 
     for autor in datos_por_autor:
-        lineas_totales_modulo = 0
+        lineas_totales_autor = 0
         print(f'\n{autor}\n\n\tFuncion{" " * (50-len("Funcion"))}Lineas\n\n\t{"=" * (50+len("Funcion"))}\n')
         grabar_linea(archivo_datos, f'{autor if "Autor" in autor else "Sin Autor"}\n\n\tFuncion{" " * (50-len("Funcion"))}Lineas\n\n\t{"=" * (50+len("Funcion"))}\n')
         
-        for funcion, lineas in datos_por_autor[autor].items():
-            lineas_totales_modulo += lineas
-            print(f'\t{funcion}{" " * (50-len(funcion))}{lineas}')
-            grabar_linea(archivo_datos, f'\t{funcion}{" " * (50-len(funcion))}{lineas}\n')
+        for funcion, cantidad_lineas in datos_por_autor[autor].items():
+            lineas_totales_autor += cantidad_lineas
+            print(f'\t{funcion}{" " * (50-len(funcion))}{cantidad_lineas}')
+            grabar_linea(archivo_datos, f'\t{funcion}{" " * (50-len(funcion))}{cantidad_lineas}\n')
+        lineas_totales_codigos += lineas_totales_autor
         
-        porcentaje_lineas_modulo = -1
+        porcentaje_lineas_modulo = round(obtener_porcentaje_lineas_codigo(datos_por_autor, lineas_totales_autor), 2)
+        lineas_totales_modulo = -1
         string = f'{len(datos_por_autor[autor])} Funciones - Lineas'
-        print(f'\t{string}{" " * (50-len(string))}{lineas_totales_modulo}\t{porcentaje_lineas_modulo}%\n\n')
-        grabar_linea(archivo_datos, f'\t{string}{" " * (50-len(string))}{lineas_totales_modulo}\t{porcentaje_lineas_modulo}%\n\n')
-    
+        print(f'\t{string}{" " * (50-len(string))}{lineas_totales_autor}\t{porcentaje_lineas_modulo}%\n\n')
+        grabar_linea(archivo_datos, f'\t{string}{" " * (50-len(string))}{lineas_totales_autor}\t{porcentaje_lineas_modulo}%\n\n')
     archivo_datos.close()
 
 def main():
