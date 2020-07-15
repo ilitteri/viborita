fb99f955824191850deee15780a4ec965f4345c8
Andreskub <akubler@fi.uba.ar>
2020-07-14 23:04:23 -0300

    Planteo la impresion de las funciones por pantalla (A Terminar)

--- punto_4.py
+++ punto_4.py
@@ -44,21 +44,59 @@ def crear_diccionario(lista_de_lineas,nombres_funciones):
                 funciones_llamadas.append(funcion)
         
         #Agrego los datos encontrados al diccionario 
-        dicc_funciones[nombre_funcion] = [cantidad_lineas,funciones_llamadas]
+        dicc_funciones[nombre_funcion] = {"lineas":cantidad_lineas,"funciones":funciones_llamadas}
 
     return dicc_funciones,funcion_main
 
-def printear_funciones(diccionario_informacion,funcion_principal):
-    encontrar = diccionario_informacion[funcion_principal]
-    print(encontrar)
+def printear_modulado(diccionario_informacion,funcion,string):
+    '''[Autor: Andrés Kübler]
+    [Ayuda: Esta funcion recorre las funciones internas de la funcion que le pasemos, e imprime
+    ordenadamente las mismas, indicando a que funcion pertenece y su cantidad de lineas]'''
 
+    #Recorro las funciones internas a la funcion recibida por parametro
+    for funcion_interna in diccionario_informacion[funcion]["funciones"]:
+        #Me fijo si la funcion interna tiene funciones dentro
+        if diccionario_informacion[funcion_interna]["funciones"] != []:
+            #Si la funcion interna recorrida es la primera, agrego su nombre y lineas al string
+            if funcion_interna == diccionario_informacion[funcion]["funciones"][0]:
+                string += " --> {} ({})".format(funcion_interna,diccionario_informacion[funcion_interna]["lineas"])
+            #Si no es la primera
+            else:
+                string += ((" " * len(string)) + " --> {} ({})".format(funcion_interna,diccionario_informacion[funcion_interna]["lineas"]))
+            
+            printear_modulado(diccionario_informacion,funcion_interna,string)
+        else:
+            print((" " * len(string)) + " --> {} ({})".format(funcion_interna,diccionario_informacion[funcion_interna]["lineas"]))
+
+
+def organizar_printeos(diccionario_informacion,funcion_principal):
+    '''[Autor: Andrés Kübler]
+    [Ayuda: Esta funcion recorre las funciones internas del main del archivo, y si estas poseen funciones
+    internas, las direcciona a la funcion printear_modulado()]'''
+
+    #Recorro las funciones del main del archvio/diccionario
+    for funcion_interna_main in diccionario_informacion[funcion_principal]["funciones"]:
+        #Me armo un string con el nombre y lineas de la función recorrido
+        string_a_imprimir = " --> {} ({})".format(funcion_interna_main,diccionario_informacion[funcion_interna_main]["lineas"])
+        #Me fijo si la funcion interna llama a otras, de ser asi la recorro en printear_modulado(), sino la imprimo
+        if diccionario_informacion[funcion_interna_main]["funciones"] != []:
+            printear_modulado(diccionario_informacion,funcion_interna_main,string_a_imprimir)
+        else:
+            print(string_a_imprimir)
 
 def main_punto4():
+    '''[Autor: Andrés Kübler]
+    [Ayuda: Esta funcion es el main del punto 4, lee el archvio fuente_unico.csv e imprime
+    por pantalla sus funciones indicando cual llama a cual otra]'''
+
+    #Leo el archivo fuente_unico.csv
     lista_de_lineas = leer_archivo()
+    #Listo las funciones del archivo
     nombres_funciones = listar_funciones(lista_de_lineas)
-    diccionario_informacion,funcion_principal = crear_diccionario(lista_de_lineas,nombres_funciones)
+    #Creo un diccionario con la informacion a utilizar del archivo
+    diccionario_informacion,fun_principal = crear_diccionario(lista_de_lineas,nombres_funciones)
     
     #En construcción
-    printear_funciones(diccionario_informacion,funcion_principal)
+    organizar_printeos(diccionario_informacion,fun_principal)
 
 main_punto4()
