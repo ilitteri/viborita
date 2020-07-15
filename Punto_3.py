aa33bd94d9fadf3688c9e3963665a049a68e8c3a
lfaguilera <67346702+lfaguilera@users.noreply.github.com>
2020-07-15 01:30:57 -0300

    Muchas correcciones en la vreacion del txt

--- Punto_3.py
+++ Punto_3.py
@@ -8,34 +8,34 @@ def buscar_invocaciones(archivo_fuente):
         nombres = invocaciones.readline()
         #Recorro las lineas del archivo 
         while nombres :
-            nombre = nombres.split('","')[0]
+            nombre = (nombres.split('","')[0]).replace('"','')
             #Almaceno los nombres de las funciones como keys del diccionario
             #Para poder identificarlos de encontrarse entre las lineas de codigo
-            diccionario_invocaciones[nombre] = []
-    
-            funciones = invocaciones.readline()  
+            diccionario_invocaciones[nombre] = [nombre]
         
+
+    
+            nombres = invocaciones.readline()  
+    print("\n" , diccionario_invocaciones , "\n")    
+
+    with open ( archivo_fuente , "r") as invocaciones :
         lineas = invocaciones.readline()
         #Recorro nuevamente el archivo para identificar invocaciones a funcione
         while lineas :
             #Identifico que funcion es la que llama a las siguientes
-            nombre = lineas.split('","')[0]
+            nombre = (lineas.split('","')[0]).replace('"','')
             #Separo las lineas que contienen codigo
             llamadas = lineas.split('","')[3:]
-
             for llamada in llamadas :
                 #Separo la funcion de su contenido (...)
-                funcion = llamada.split("(")[0]
-                if " " in funcion :
-                    funcion_limpia = funcion.replace(" ","")
-
-                #Busco funciones con nombres existentes en el diccionario
-                if funcion_limpia in diccionario_invocaciones:
-                    #Creo listas dentro del diccionario 
-                    diccionario_invocaciones[nombre] = []
-                    #Agrego las funciones encontradas en una lista para facilitar operaciones
-                    diccionario_invocaciones[nombre].append(funcion_limpia)
+                for funcion in diccionario_invocaciones :
+                    if funcion in llamada and funcion != nombre :
+                        
+                        diccionario_invocaciones[nombre].append(funcion)
+            
             lineas = invocaciones.readline()
+    print ("\n" , diccionario_invocaciones , "\n" )
+
     return diccionario_invocaciones
 
 def contar_interacciones(diccionario_invocaciones):
@@ -43,11 +43,16 @@ def contar_interacciones(diccionario_invocaciones):
     
     #Creo un nuevo diccionario que tiene las funciones sin repetir 
     diccionario_funciones = {}
-    for funcion in diccionario_invocaciones :
+    for key in diccionario_invocaciones :
+
         #Cuento su numero de llamadas
-        for invocacion in diccionario_invocaciones[funcion]:
-            numero_llamadas = diccionario_invocaciones[funcion].count(invocacion)
-            diccionario_funciones[funcion][invocacion] =  numero_llamadas
+    
+        for invocacion in diccionario_invocaciones[key]:
+            if invocacion is not key :
+                diccionario_funciones [key] = {invocacion : diccionario_invocaciones[key].count(invocacion)}
+            else :
+                diccionario_funciones [key] = {invocacion : 0}
+    print (diccionario_funciones)
     return diccionario_funciones
 
 def creacion_formato_tabla(diccionario_funciones):
@@ -56,48 +61,55 @@ def creacion_formato_tabla(diccionario_funciones):
     texto_max = 0
     numero_tabla = 1
     filas_txt = []
-    filas_txt [0] = ""
     indice = 1
-    indice_2 = 0
+    indice_2 = 1
+    indice_3 = 1
+    invocacion = 0
     total_invocado = []
+    relacion_indices = {}
     #Esta funcion sirve para que los nommbres no queden demaciado largos para el formato que se quiere
-    for funcion in diccionario_funciones.keys():
-        if len(funcion+"\t") > texto_max :
-            texto_max = len(funcion)
-            correccion_espaciado = " " * (texto_max - len(funcion))
-            numero_tabla += 1
-        filas_txt.append(str( " ",numero_tabla," - ",funcion,correccion_espaciado, "\t"*2 ))
-        while indice < len (diccionario_funciones[funcion]) :
-            if diccionario_funciones[funcion][indice_2] != 0 :
-                filas_txt[indice_2+1] = " " * 3
-                total_invocado.append(0)
-            else :
-                filas_txt[indice_2+1] = " " + str(diccionario_funciones[funcion][indice_2]) + " "
-                total_invocado.append(diccionario_funciones[funcion][indice_2])
-            indice_2 += 1
-    filas_txt [0] = (str(" ","FUNCIONES",correccion_espaciado, "\t"*2))
+    filas_txt.append(str(" FUNCIONES" + " "*20 ))
+    for numero in range(len (diccionario_funciones)):
+        filas_txt [0] += "\t" + str(numero+1) + "\t" 
+    filas_txt[0] += " \n"
 
-    while indice <= numero_tabla :
-        filas_txt[0] += " "+str(indice)+" "
-        indice += 1 
-    correccion_espaciado_total = texto_max
-    filas_txt [numero_tabla+1] = str("Total Invocaciones",correccion_espaciado,"\t"*2 )
-    for invocaciones_count in total_invocado :
-        filas_txt [numero_tabla+1] = filas_txt[numero_tabla+1] + " " + invocaciones_count + " "
+    for funcion in diccionario_funciones :
+        relacion_indices [indice_3] = str(funcion)
+        indice_3 += 1
+        for invocacion in diccionario_funciones[funcion] : 
+            cadena_de_texto =  "\t" + " " + str(indice_2) + " " + str(invocacion) + " " + " " * (20-len(str(invocacion)) )
+            filas_txt.append(cadena_de_texto )
+            indice_2 += 1  
+        
+    #print(relacion_indices)  
+    
+    return filas_txt , relacion_indices
+
+def asignacion_valores_tabla (filas_txt, relacion_indices , diccionario_funciones) : 
+    suma_filas = {}
+    for fila in range (1,len(filas_txt)) :
+        for funcion in diccionario_funciones[relacion_indices[fila]] :
+        
+            cantidad = int(diccionario_funciones[relacion_indices[fila]][funcion])
+            
+            filas_txt [fila] += ("\t" + str(cantidad) + "\t") 
+            suma_filas = {fila : cantidad}
     return filas_txt
-
+    
 def creacion_archivo_txt (filas_txt) :
     """[Autor: Luciano Federico Aguilera]"""
     # Escribo el archivo de texto txt con las lineas ordenadas
     with open("analizador.txt" , "w") as analizador :
+
         for linea in filas_txt:
-            analizador.write(linea)
+            analizador.write(linea + "\n")
 
 def main () :
     archivo_fuente = "fuente_unico.csv"
     diccionario_invocaciones = buscar_invocaciones(archivo_fuente)
     diccionario_funciones = contar_interacciones(diccionario_invocaciones)
-    filas_txt = creacion_formato_tabla(diccionario_funciones)
+    filas_txt , relacion_indices = creacion_formato_tabla(diccionario_funciones)
+    filas_txt = asignacion_valores_tabla (filas_txt, relacion_indices , diccionario_funciones)
     creacion_archivo_txt (filas_txt)
 
 main ()
