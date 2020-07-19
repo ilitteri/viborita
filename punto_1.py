c34c4dd56ef4f5fe09abc22552158a1b820bde09
SantiagoVaccarelli <67487718+SantiagoVaccarelli@users.noreply.github.com>
2020-07-19 00:25:31 -0300

    Escribi una funcion para sacar las longitudes maximas de cada columna, y una para imprimir la tabla.

--- punto_1.py
+++ punto_1.py
@@ -1,42 +1,43 @@
-'''Mediante esta opción se debe mostrar por pantalla, una tabla con la siguiente información
-por columna:
+import m_organizar_datos as organizar_datos
 
-Nombre de la Función.Módulo (FUNCION) – nombre función seguido de un punto y el nombre del módulo
-Cantidad de Parámetros Formales (Parámetros)
-Cantidad de líneas de código (Líneas) – no contabilizar las líneas vacías
-Cantidad de invocaciones a la función (Invocaciones)
-Cantidad de Puntos de Salida (Returns)
-Cantidad de If, contabilizar los anidamientos elif (If/elif)
-Cantidad de For (for)
-Cantidad de While (while)
-Cantidad de Break (Break)
-Cantidad de Exit (Exit)
-Cantidad de líneas de comentarios (Coment) – sólo los extra descripción
-Indicador de Descripción Función (Ayuda) – Si/No indicando que hay descripción de uso de la función, para lo cual se debe evaluar si inmediatamente después de la firma de la función, existe comentario entre comillas triples.
-Autor/Responsable (Autor)
-Utilice como nombre de las columnas, los nombres que figuran entre paréntesis en cada
-caso.
+def longitudes_maximas(columnas_datos, longitud):
+    for elemento in range(len(columnas_datos)):
+        if len(columnas_datos[elemento]) > longitud[elemento]:
+            longitud[elemento] = len(columnas_datos[elemento])
+    
+    return longitud
 
-Este punto también debe generar el archivo “panel_general.csv”, en el cual cada línea del
-archivo contenga la información descripta en cada uno de los puntos.
-La primera línea del archivo, debe contener los nombres que figuran entre paréntesis.'''
-
+def imprimir(lista_de_listas, longitud):
+    for lista in range(len(lista_de_listas)):
+        for elemento in range(len(lista_de_listas[lista])):
+            lista_de_listas[lista][elemento] += (" " * (longitud[elemento] - len(lista_de_listas[lista][elemento])))
+        print("\t".join(lista_de_listas[lista]))
+    
 def grabar(diccionario):
+    longitud = [0,0,0,0,0,0,0,0,0,0,0,0,0]
+    columnas_titulo = ["Funcion", "Parametros", "Lineas", "Invocaciones", "Returns", "If/Elif", "For", "While", "Break", "Exit", "Coment", "Ayuda", "Autor"]
+    longitud = longitudes_maximas(columnas_titulo,longitud)
+
+    lista_de_listas = [columnas_titulo]
     with open("panel_general.csv", "w") as panel_general:
-            panel_general.write("funcion, parametros, lineas, invocaciones, returns, if/elif, for, while, break, exit, coment, ayuda, autor \n")
-            for key in diccionario:
-                panel_general.write(f'{key}.{diccionario[key]["modulo"]},{diccionario[key]["parametros"]},{diccionario[key]["lineas"]},{diccionario[key]["invocaciones"]},{diccionario[key]["returns"]},{diccionario[key]["if/elif"]},{diccionario[key]["for"]},{diccionario[key]["while"]},{diccionario[key]["break"]},{diccionario[key]["exit"]},{diccionario[key]["coment"]},{"si" if diccionario[key]["ayuda"] else "no"},{diccionario[key]["autor"]}\n')
+        panel_general.write("funcion, parametros, lineas, invocaciones, returns, if/elif, for, while, break, exit, coment, ayuda, autor \n")
+        for key in diccionario:    
+            columnas_datos = [f'{key}.{diccionario[key]["modulo"]}', f'{diccionario[key]["parametros"]}', 
+                            f'{diccionario[key]["lineas"]}', f'{diccionario[key]["invocaciones"]}', 
+                            f'{diccionario[key]["returns"]}', f'{diccionario[key]["if/elif"]}', 
+                            f'{diccionario[key]["for"]}', f'{diccionario[key]["while"]}', 
+                            f'{diccionario[key]["break"]}', f'{diccionario[key]["exit"]}',
+                            f'{diccionario[key]["coment"]}', 
+                            f'{"Si" if diccionario[key]["ayuda"] else "No"}', f'{diccionario[key]["autor"]}']
+            lista_de_listas.append(columnas_datos)       
+            panel_general.write(f'{columnas_datos[0]},{columnas_datos[1]},{columnas_datos[2]},{columnas_datos[3]},{columnas_datos[4]},{columnas_datos[5]},{columnas_datos[6]},{columnas_datos[7]},{columnas_datos[8]},{columnas_datos[9]},{columnas_datos[10]},{columnas_datos[11]}\n')
+            longitud = longitudes_maximas(columnas_datos,longitud)
 
-
+    imprimir(lista_de_listas,longitud)
 
 def main():
-    from m_organizar_datos import por_cantidad_declaraciones_funcion
     with open ("fuente_unico.csv", "r") as fuente_unico, open ("comentarios.csv", "r") as comentarios: 
-        diccionario = por_cantidad_declaraciones_funcion(fuente_unico, comentarios)
+        diccionario = organizar_datos.por_cantidad_declaraciones_funcion(fuente_unico, comentarios)
     grabar(diccionario)
 
 main()
-
-
-
-
