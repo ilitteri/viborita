2e44ca444604729fc692fdb18b40189b6b0e2dec
Andreskub <akubler@fi.uba.ar>
2020-07-20 13:41:26 -0300

    Modifico m_arbol_invocacion.py: agrego funciones repetidas y modifico impresion

--- m_arbol_invocacion.py
+++ m_arbol_invocacion.py
@@ -1,3 +1,4 @@
+  
 def leer_archivo():
     '''[Autor: Andrés Kübler]
     [Ayuda: Lee el archivo .csv y devuelve sus lineas en forma de lista]'''
@@ -27,7 +28,7 @@ def crear_diccionario(lista_de_lineas,nombres_funciones):
 
     #Crea diccionario vacio donde va a ir guardando la información extraida
     dicc_funciones = {}
-
+    
     #Recorre la lista
     for linea in lista_de_lineas:
         #Convierte la linea en una lista para asi recorrerla por índices
@@ -41,27 +42,30 @@ def crear_diccionario(lista_de_lineas,nombres_funciones):
         cantidad_lineas = len(linea_listada) - 3
         
         #Crea una lista vacia y agrega las funciones internas que llama la función evaluada
-        funciones_llamadas = []
-        for funcion in nombres_funciones:
-            if (funcion in linea[2:]) and (funcion not in funciones_llamadas):
-                funciones_llamadas.append(funcion)
+        funciones_llamadas = []        
+        for elemento in linea_listada[2:]:
+            for funcion in nombres_funciones:
+                if (funcion in elemento):
+                    funciones_llamadas.append(funcion)
         
         #Agrega los datos extraidos al diccionario 
         dicc_funciones[nombre_funcion] = {"lineas":cantidad_lineas,"funciones":funciones_llamadas}
-
+    
     return dicc_funciones,funcion_main
 
 def recorrer_funcion(diccionario_informacion,funcion,string):
     '''[Autor: Andrés Kübler]
     [Ayuda: Recorre las funciones internas de la funcion que le pasemos, e imprime
     ordenadamente las mismas, indicando a que funcion pertenece y su cantidad de lineas]'''
+    funciones_recorridas = []
 
     #Recorre las funciones internas a la funcion recibida por parametro
     for funcion_interna in diccionario_informacion[funcion]["funciones"]:
+        funciones_recorridas.append(funcion_interna)
         #Comprueba si la funcion interna llama a otras funciones
         if diccionario_informacion[funcion_interna]["funciones"] != []:
             #Si la funcion interna recorrida es la primera, agrega su nombre y lineas al string
-            if funcion_interna == diccionario_informacion[funcion]["funciones"][0]:
+            if (funcion_interna == diccionario_informacion[funcion]["funciones"][0]) and (funciones_recorridas.count(funcion_interna) == 1):
                 string += " --> {} ({})".format(funcion_interna,diccionario_informacion[funcion_interna]["lineas"])
             #Si no es la primera, modifica el string agregando espacios correspondientes
             else:
@@ -70,11 +74,10 @@ def recorrer_funcion(diccionario_informacion,funcion,string):
             recorrer_funcion(diccionario_informacion,funcion_interna,string)
         #Si funcion_interna no llama a otras funciones organiza su impresión
         else:
-            if funcion_interna == diccionario_informacion[funcion]["funciones"][0]:
+            if (funcion_interna == diccionario_informacion[funcion]["funciones"][0]) and (funciones_recorridas.count(funcion_interna) == 1):
                 print(string + " --> {} ({})".format(funcion_interna,diccionario_informacion[funcion_interna]["lineas"]))
             else:
                 print((" " * len(string)) + " --> {} ({})".format(funcion_interna,diccionario_informacion[funcion_interna]["lineas"]))
-
 
 def organizar_impresion(diccionario_informacion,funcion_principal):
     '''[Autor: Andrés Kübler]
@@ -83,17 +86,21 @@ def organizar_impresion(diccionario_informacion,funcion_principal):
 
     #Crea una variable donde guarda la longitud del string de funcion_principal    
     longitud = 0
+    funciones_recorridas = []
 
     #Recorre las funciones de la funcion principal del archvio/diccionario
     for funcion_interna_main in diccionario_informacion[funcion_principal]["funciones"]:
+        funciones_recorridas.append(funcion_interna_main)
         #Crea un string con el nombre y cantidad de lineas de la funcion_principal
         string_a_imprimir = " --> {} ({})".format(funcion_interna_main,diccionario_informacion[funcion_interna_main]["lineas"])
         
         #Prueba si la funcion_interna_main recorrida es la primera de la funcion_principal,si lo es agrega el nombre de la funcion y cantidad lineas antes del string de funcion_principal
-        if (funcion_interna_main == diccionario_informacion[funcion_principal]["funciones"][0]):
+        if (funcion_interna_main == diccionario_informacion[funcion_principal]["funciones"][0]) and (funciones_recorridas.count(funcion_interna_main) == 1):
             string_funcion_principal = "--> {} ({})".format(funcion_principal,diccionario_informacion[funcion_principal]["lineas"])
             longitud += len(string_funcion_principal)
             string_a_imprimir = string_funcion_principal + string_a_imprimir
+        elif(funcion_interna_main == diccionario_informacion[funcion_principal]["funciones"][0]) and (funciones_recorridas.count(funcion_interna_main) > 1):
+            string_a_imprimir = (" " * len("--> {} ({})".format(funcion_principal,diccionario_informacion[funcion_principal]["lineas"]))) + " --> {} ({})".format(funcion_interna_main,diccionario_informacion[funcion_interna_main]["lineas"])
         #Si no es la primera función, imprime su equivalente en longitud en espacios blancos
         elif (funcion_interna_main != diccionario_informacion[funcion_principal]["funciones"][0]):
             string_a_imprimir = (" " * longitud) + " --> {} ({})".format(funcion_interna_main,diccionario_informacion[funcion_interna_main]["lineas"])
@@ -117,5 +124,3 @@ def main():
     diccionario_informacion,fun_principal = crear_diccionario(lista_de_lineas,nombres_funciones)
     #Analiza las funciones del diccionario e imprime un diagrama de las mismas y sus funciones internas
     organizar_impresion(diccionario_informacion,fun_principal)
-    print(diccionario_informacion)
-main()
