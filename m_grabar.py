1a2a4cc9407f465ba3c8a9db07e096aa17b95977
Ivan Litteri (FIUBA) <ilitteri@fi.uba.ar>
2020-07-23 18:20:27 +0200

    Comento

--- m_grabar.py
+++ m_grabar.py
@@ -1,8 +1,10 @@
 def cadena(archivo, linea):
-    '''[Autores: Luciano Aguilera, Ivan Litteri]'''
+    '''[Autores: Ivan Litteri]
+    [Ayuda: Graba una linea en el archivo que le llega por parametro]'''
     archivo.write(linea)
 
 def cadenas(archivo, lineas):
-    '''[Autor: Ivan Litteri]'''
+    '''[Autor: Ivan Litteri]
+    [Ayuda: Graba una lista de lineas en el archivo que le llega por parametro]'''
     for linea in lineas:
         archivo.write(linea)
