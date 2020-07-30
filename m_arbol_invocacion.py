88233e8c7e2b27d8303e115fda362b6a29830f57
Andreskub <akubler@fi.uba.ar>
2020-07-30 15:38:42 -0300

    Agrego condicion de excepcion si no encuentra funcion principal

--- m_arbol_invocacion.py
+++ m_arbol_invocacion.py
@@ -22,7 +22,7 @@ def encontrar_main_archivo(diccionario_informacion, funcion_main_dicc = None):
 
     return funcion_main_dicc
 
-def grafica_arbol_invocaciones(diccionario_informacion, funcion = None, string = ""):
+def procesar_impresion_arbol(diccionario_informacion, funcion = None, string = ""):
     '''[Autor: Andrés Kübler]
     [Ayuda: Esta función recursiva analiza las invocaciones de cada función del diccionario. Recibe una funcion y un string como parametros. Minetras
     este en funcionamiento imprime las invocaciones con sus respectivas cantidades de lineas]'''
@@ -51,7 +51,18 @@ def grafica_arbol_invocaciones(diccionario_informacion, funcion = None, string =
                 print(f'{" " * len(str_invocacion)} ---> {invocaciones[invocacion_de_funcion]}({diccionario_informacion[funcion]["cantidad_lineas"]})')
             #Si la invocacion recorrida es la primera de las invocaciones, la función se llama a si misma y pasa el string como variable
             elif (invocacion_de_funcion == 0):
-                grafica_arbol_invocaciones(diccionario_informacion, invocaciones[invocacion_de_funcion], str_invocacion)
+                procesar_impresion_arbol(diccionario_informacion, invocaciones[invocacion_de_funcion], str_invocacion)
             #Sino es la primera, se llama a si misma y pasa la longitud del string pero en espacios
             else:
-                grafica_arbol_invocaciones(diccionario_informacion, invocaciones[invocacion_de_funcion], " " * len(str_invocacion))
+                procesar_impresion_arbol(diccionario_informacion, invocaciones[invocacion_de_funcion], " " * len(str_invocacion))
+
+def grafica_arbol_invocaciones(diccionario_informacion):
+    '''[Autor: Andrés Kübler]
+    [Ayuda: Esta es la funcion principal del punto. Recorre el diccionario, simepre y cuando exista una funcion
+    principal. Si no la encuentra imprime error]'''
+
+    try:
+        procesar_impresion_arbol(diccionario_informacion)
+    #Exceptua el error si no encuentra el main
+    except Exception:
+        print("¡ERROR: NO SE ENCONTRÓ FUNCIÓN PRINCIPAL!")
