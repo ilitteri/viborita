b4fc9b40ebe5fcd53e7d0b653f2eb7e6b3dca093
Ivan Litteri (FIUBA) <ilitteri@fi.uba.ar>
2020-07-30 23:26:11 +0200

    Saco la deteccion de funciones repetidas ya que no va a haber2

--- m_organizar_datos.py
+++ m_organizar_datos.py
@@ -1,6 +1,6 @@
 import m_obtener as obtener
 
-def actualizar_diccionario_funciones(datos_por_funciones, nombre_funcion, parametros_funcion, modulo_funcion, lineas_funcion, autor_funcion, ayuda_funcion, otros_c, indice_copia):
+def actualizar_diccionario_funciones(datos_por_funciones, nombre_funcion, parametros_funcion, modulo_funcion, lineas_funcion, autor_funcion, ayuda_funcion, otros_c):
     '''[Autor: Ivan Litteri]
     [Ayuda: recibe los datos extraidos de una linea leida de los archivos csv y los guarda en el diccionario final.]'''
 
@@ -15,23 +15,6 @@ def actualizar_diccionario_funciones(datos_por_funciones, nombre_funcion, parame
                                                         "cantidad_parametros": 0,
                                                         "cantidad_comentarios": len(otros_c),
                                                         }
-    #Si la funcion si esta como key del diccionario significa que hay una funcion repetida, entonces la agrega al diccionario pero con un subindice
-    elif (nombre_funcion in datos_por_funciones):
-        if (f'{nombre_funcion}_{indice_copia-1}' in datos_por_funciones):
-            nombre_funcion += f'_{indice_copia}'
-        else:
-            indice_copia = 0
-            nombre_funcion += f'_{indice_copia}'
-        indice_copia += 1
-        datos_por_funciones[nombre_funcion] = {"parametros": None,
-                                                "modulo": None,
-                                                "lineas": None,
-                                                "cantidad_lineas": 0,
-                                                "invocaciones": [],
-                                                "cantidad_invocaciones": 0,
-                                                "cantidad_parametros": 0,
-                                                "cantidad_comentarios": len(otros_c) if otros_c[0] != '"\n' else 0,
-                                                }
     #Agrego los datos a sus respectivos lugares
     datos_por_funciones[nombre_funcion]["parametros"] = parametros_funcion if len(parametros_funcion) > 2 else None
     datos_por_funciones[nombre_funcion]["modulo"] = modulo_funcion
@@ -57,9 +40,9 @@ def actualizar_diccionario_funciones(datos_por_funciones, nombre_funcion, parame
                                                                                     }
     obtener.cantidad_declaraciones(datos_por_funciones, lineas_funcion, nombre_funcion)
 
-    return datos_por_funciones, indice_copia
+    return datos_por_funciones
 
-def actualizar_diccionario_autores(datos_por_autores, nombre_funcion, lineas_funcion, autor_funcion, indice_copia):
+def actualizar_diccionario_autores(datos_por_autores, nombre_funcion, lineas_funcion, autor_funcion):
     '''[Autor: Ivan Litteri]
     [Ayuda: recibe los datos extraidos de una linea leida de los archivos csv y los guarda en el diccionario final.]'''
 
@@ -72,7 +55,7 @@ def actualizar_diccionario_autores(datos_por_autores, nombre_funcion, lineas_fun
         datos_por_autores[autor_funcion]["funciones"][nombre_funcion] = len(lineas_funcion)
         datos_por_autores[autor_funcion]["lineas_totales"] += len(lineas_funcion)
             
-    return datos_por_autores, indice_copia
+    return datos_por_autores
 
 def leer_archivos_csv(archivo_fuente, archivo_comentarios):
     '''[Autor: Ivan Litteri]
@@ -83,8 +66,6 @@ def leer_archivos_csv(archivo_fuente, archivo_comentarios):
     datos_por_autores = {}
     #Declaro el diccionario vacio para llenar luego
     datos_por_funciones = {}
-
-    indice_copia = 0
 
     #Cargo la primera linea del archivo fuente
     linea_fuente = archivo_fuente.readline().rstrip('"\n')
@@ -98,8 +79,8 @@ def leer_archivos_csv(archivo_fuente, archivo_comentarios):
         nombre_funcion_c, autor_funcion, ayuda_funcion, *otros_c = linea_comentarios.split('","')
         nombre_funcion = nombre_funcion_f = nombre_funcion_c = nombre_funcion_f.replace('"', '')
         #Actualiza los diccionarios con los datos extraidos de las lineas
-        datos_por_funciones, indice_copia = actualizar_diccionario_funciones(datos_por_funciones, nombre_funcion, parametros_funcion, modulo_funcion, lineas_funcion, autor_funcion, ayuda_funcion, otros_c, indice_copia)
-        datos_por_autores, indice_copia = actualizar_diccionario_autores(datos_por_autores, nombre_funcion, lineas_funcion, autor_funcion, indice_copia)
+        datos_por_funciones = actualizar_diccionario_funciones(datos_por_funciones, nombre_funcion, parametros_funcion, modulo_funcion, lineas_funcion, autor_funcion, ayuda_funcion, otros_c)
+        datos_por_autores = actualizar_diccionario_autores(datos_por_autores, nombre_funcion, lineas_funcion, autor_funcion)
         #Avanzo de linea en el archivo
         linea_fuente = archivo_fuente.readline().rstrip('"\n')
         #Avanzo de linea en el archivo
