64412062728ff0f3d6cdd2c302e482ae27ecaada
Ivan Litteri (FIUBA) <ilitteri@fi.uba.ar>
2020-08-02 21:59:43 +0200

    Abro los archivos csv, los leo y guardo las funciones como objetos

--- readCSV.py
+++ readCSV.py
@@ -1,8 +1,18 @@
 class Function:
     def __init__(self, sourceSplitedLine: str, commentSplittedLine: str):
         self.name = sourceSplitedLine[0][2:] if '*' in sourceSplitedLine[0] else sourceSplitedLine[0][1:]
-        self.parameters = sourceSplitedLine[1]
+        self.parameters = sourceSplitedLine[1][1:-1]
         self.lines = sourceSplitedLine[3:]
         self.author = commentSplittedLine[1]
         self.help = commentSplittedLine[2]
         self.comments = commentSplittedLine[3:]
+
+functions = []
+
+with open('fuente_unico.csv', 'r') as sourceCSV, open('comentarios.csv', 'r') as commentsCSV:
+    sourceLine = sourceCSV.readline().rstrip('\n')
+    commentsLine = commentsCSV.readline().rstrip('\n')
+    while sourceLine and commentsLine:
+        functions.append(Function(sourceLine.split('","'), commentsLine.split('","')))
+        sourceLine = sourceCSV.readline().rstrip('\n')
+        commentsLine = commentsCSV.readline().rstrip('\n')
