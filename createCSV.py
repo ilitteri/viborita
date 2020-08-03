6b02109ae4009f7828f811c1111c94b4a7f843c7
Ivan Litteri (FIUBA) <ilitteri@fi.uba.ar>
2020-08-03 23:00:08 +0200

    Modifico el analisis de los comentarios multilinea

--- createCSV.py
+++ createCSV.py
@@ -37,58 +37,53 @@ def readFirstLines(openedFiles):
         
     return lines
 
-def analyzeComment(comentsCSV, file, line, multiLineFlag=False):
+def getTripleLineComment(file, line):
+    comment = ''
+    while not line.rstrip().endswith("'''"):
+        comment += line.lstrip()
+        line = readLine(file)
+    comment += line.lstrip()
+    print(comment)
+    return comment
 
-    if multiLineFlag:
-        if line.count("'''") == 2:
-            if 'Autor' in line:
-                comentsCSV.write(f',"{line[line.index("[") + 1:line.index("]")]}"')
-            elif 'Ayuda' in line:
-                comentsCSV.write(f',"{line[line.index("[") + 1:line.index("]")]}"')
-            else:
-                comentsCSV.write(f',"{line}"')
-        elif line.count("'''") == 1:
-            while not line.rstrip().endswith("'''"):
-                if 'Autor' in line:
-                    comentsCSV.write(f',"{line[line.index("[") + 1:line.index("]")]}"')
-                elif 'Ayuda' in line:
-                    comentsCSV.write(f',"{line[line.index("[") + 1:] if not "]" in line else line[:line.index("]")].lstrip()}')
-                else:
-                    comentsCSV.write(f'{line.lstrip() if not "]" in line else line[:line.index("]")].lstrip()}')
-                line = readLine(file)
-            if line.lstrip() != "'''":
-                comentsCSV.write(f'{line[:line.index("]")]}')
-            comentsCSV.write('"')
+def analyzeMultiLineComment(comentsCSV, file, line, multiLineFlag=True):
+    comment = getTripleLineComment(file, line)
+    authorIndex = comment.find('Autor:')
+    helpIndex = comment.find('Ayuda:')
+    if authorIndex != -1:
+        author = comment[authorIndex:comment.index(']')]
     else:
-        comentsCSV.write(f',"{line[line.index("#"):]}"')
-
-    line = readLine(file)
-
-    return line
+        author = 'Sin Autor'
+    if helpIndex != -1:
+        help = comment[helpIndex:comment.index(']', helpIndex)]
+    else:
+        help = ''
+    
+    return author, help
 
 def writeCSV(sourceCSV, comentsCSV, file, line, outOfFunctionLines, fileName):
 
     function = line[4:line.index('(')]
     parameters = line[line.index('(') + 1:line.index(')')]
-    module = fileName
     if line[4:line.index(')') + 1] in outOfFunctionLines:
-        sourceCSV.write(f'"*{function}","{parameters}","{module}"')
+        sourceCSV.write(f'"*{function}","{parameters}","{fileName}"')
     else:
-        sourceCSV.write(f'"{function}","{parameters}","{module}"')
+        sourceCSV.write(f'"{function}","{parameters}","{fileName}"')
     comentsCSV.write(f'"{function}"')
 
     line = readLine(file)
     while line != chr(255) and not line.startswith('def '):
         if line.lstrip().startswith("'''"):
-            line = analyzeComment(comentsCSV, file, line, multiLineFlag=True)
+            author, help = analyzeMultiLineComment(comentsCSV, file, line)
+            comentsCSV.write(f',"{author}","{help}"')
         elif line.lstrip().startswith('#'):
             comentsCSV.write(f',"{line.rstrip()}"')
-            line = readLine(file)
         elif '#' in line and not ('"#"' in line and '#todo' in line):
-            line = analyzeComment(comentsCSV, file, line, multiLineFlag=False)
+            sourceCSV.write(f',"{line[:line.index("#")]}"')
+            comentsCSV.write(f',"{line[line.index("#"):]}"')
         else:   
             sourceCSV.write(f',"{line.rstrip()}"')
-            line = readLine(file)
+        line = readLine(file)
 
     sourceCSV.write('\n')
     comentsCSV.write('\n')
