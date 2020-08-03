fa9d6d24b8a47adab1b7813d77e16ac545c7893d
Ivan Litteri (FIUBA) <ilitteri@fi.uba.ar>
2020-08-03 04:41:02 +0200

    Agrego algunos atributos

--- readCSV.py
+++ readCSV.py
@@ -1,11 +1,14 @@
 class Function:
     def __init__(self, sourceSplitedLine: str, commentSplittedLine: str):
         self.name = sourceSplitedLine[0][2:] if '*' in sourceSplitedLine[0] else sourceSplitedLine[0][1:]
-        self.parameters = sourceSplitedLine[1][1:-1].split(',')
+        self.parameters = sourceSplitedLine[1].split(',')
         self.lines = sourceSplitedLine[3:]
         self.author = commentSplittedLine[1]
         self.help = commentSplittedLine[2]
         self.comments = commentSplittedLine[3:]
+        self.linesCant = len(self.lines)
+        self.parametersCant = len(self.parameters)
+        self.commentsCant = len(self.comments)
 
 functions = []
 
@@ -16,3 +19,6 @@ with open('fuente_unico.csv', 'r') as sourceCSV, open('comentarios.csv', 'r') as
         functions.append(Function(sourceLine.split('","'), commentsLine.split('","')))
         sourceLine = sourceCSV.readline().rstrip('\n')
         commentsLine = commentsCSV.readline().rstrip('\n')
+
+for function in functions:
+    print(function.linesCant)
