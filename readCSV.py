27005dfc641495aa2f096e59d9393ed36303e22e
Ivan Litteri (FIUBA) <ilitteri@fi.uba.ar>
2020-08-04 04:04:53 +0200

    Ahora la funcion tiene a la cantidad de declaraciones

--- readCSV.py
+++ readCSV.py
@@ -6,19 +6,40 @@ class Function:
         self.author = commentSplittedLine[1]
         self.help = commentSplittedLine[2]
         self.comments = commentSplittedLine[3:]
-        self.linesCant = len(self.lines)
-        self.parametersCant = len(self.parameters)
-        self.commentsCant = len(self.comments)
+        self.linesCount = len(self.lines)
+        self.parametersCount = len(self.parameters)
+        self.commentsCount = len(self.comments)
+        self.statements = Statements(self.lines)
 
-functions = []
+class Statements:
+    def __init__(self, lines: list):
+        self.forCounter = 0
+        self.returnCounter = 0
+        self.ifElifCounter = 0
+        self.whileCounter = 0
+        self.breakCounter = 0
+        self.exitCounter = 0
 
-with open('fuente_unico.csv', 'r') as sourceCSV, open('comentarios.csv', 'r') as commentsCSV:
-    sourceLine = sourceCSV.readline().rstrip('\n')
-    commentsLine = commentsCSV.readline().rstrip('\n')
-    while sourceLine and commentsLine:
-        functions.append(Function(sourceLine.split('","'), commentsLine.split('","')))
+        for line in self.lines:
+            if "for " in line:
+                self.forCounter += line.count("for")
+            if "return " in line:
+                self.returnCounter += 1
+            if "if " in line: 
+                self.ifElifCounter += 1
+            if "while " in line:
+                self.whileCounter += 1
+            if "break" in line:
+                self.breakCounter += 1
+            if "exit" in line:
+                self.exitCounter += 1
+
+def getFunctions():
+    functions = []
+    with open('fuente_unico.csv', 'r') as sourceCSV, open('comentarios.csv', 'r') as commentsCSV:
         sourceLine = sourceCSV.readline().rstrip('\n')
         commentsLine = commentsCSV.readline().rstrip('\n')
-
-for function in functions:
-    print(function.linesCant)
+        while sourceLine and commentsLine:
+            functions.append(Function(sourceLine.split('","'), commentsLine.split('","')))
+            sourceLine = sourceCSV.readline().rstrip('\n')
+            commentsLine = commentsCSV.readline().rstrip('\n')
